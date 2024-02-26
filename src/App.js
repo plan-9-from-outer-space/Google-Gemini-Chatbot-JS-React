f959375217dcbd59ab3db33c9e8d64f27ebf22b7
Rob Merchant <rjm288@gmail.com>
2024-02-26 18:04:19 -0500

    First version of chatbot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const surpriseOptions = [
+    'Who won the latest Nobel Peace Prize for Economics?',
+    'How do you make a pizza?',
+    'Who is the president of the Ukraine?'
+  ];
+
+  function surprise () {
+    const numOptions = surpriseOptions.length;
+    const randomValue = surpriseOptions[Math.floor(Math.random() * numOptions)];
+    setValue(randomValue);
+  }
+
+  async function getResponse () {
+    if (!value) {
+      setError("Error! Please ask a question!");
+      return;
+    }
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({ history: chatHistory, message: value }),
+        headers: { 'Content-Type': 'application/json'}
+      };
+      const response = await fetch('http://localhost:8000/gemini', options);
+      const data = await response.text();
+      console.log(data);
+      setChatHistory(oldChatHistory => [ ...oldChatHistory, 
+          { role: 'user', parts: value }, 
+          { role: 'model', parts: data } ]);
+      setValue("");
+    } catch (error) {
+        console.error(error);
+        setError("Something went wrong! Please try again later.");
+    }
+  }
+
+function clear () {
+  setValue("");
+  setError("");
+  setChatHistory([]);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>
+        What do you want to know?
+        {/* <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me</button> */}
+        <button className="surprise" onClick={surprise}>Surprise me</button>
+      </p>
+      <div className="input-container">
+        <input value={value} 
+               placeholder="When is Christmas?" 
+               onChange={(e) => setValue(e.target.value)} />
+        {!error && <button onClick={getResponse}>Ask me</button>}
+        {error && <button onClick={clear}>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="search-result">
+          {chatHistory.map( (chatItem, _index) => 
+              <div key={_index}>
+                <p className="answer">{chatItem.role} : {chatItem.parts}</p>
+              </div>
+          )}
+      </div>
     </div>
   );
 }
---
+++ src/server.js
@@ -0,0 +1,27 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEN_AI_KEY);
+
+app.post('/gemini', async (req, res) => {
+    // console.log(req.body.history);
+    // console.log(req.body.message);
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+    const chat = model.startChat({
+        history: req.body.history
+    });
+    const msg = req.body.message;
+    const result = await chat.sendMessage(msg);
+    const response = await result.response;
+    const text = response.text();
+    res.send(text); // send response back to front end
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
